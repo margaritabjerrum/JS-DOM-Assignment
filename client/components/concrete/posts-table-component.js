521d0bfb06e5a3266f6669a80d215638a2579800
margaritabjerrum <margarita.juskyte@gmail.com>
2022-12-29 15:24:42 +0200

    feat: row data edit UI/UX implemented

--- client/components/concrete/posts-table-component.js
+++ client/components/concrete/posts-table-component.js
@@ -21,23 +21,62 @@ class PostsTableComponent {
     this.renderPosts(posts);
   }
 
+  enableRowEditAction = ({ initialState, id, tr }) => {
+    let isBeingEdited = false;
+    const postColumn = tr.querySelector('.js-post-col');
+    const udpateButton = tr.querySelector('.btn-success');
+
+    const cancelEditing = () => {
+      tr.classList.remove('table-secondary');
+      editButton.innerHTML = '<i class="bi bi-pencil-square"></i>';
+      editButton.classList.replace('btn-secondary', 'btn-warning');
+      postColumn.setAttribute('contenteditable', 'false');
+      udpateButton.classList.add('d-none');
+      postColumn.textContent = initialState.post;
+      isBeingEdited = false;
+    }
+
+    const enableEditing = () => {
+      tr.classList.add('table-secondary');
+      editButton.innerHTML = '<i class="bi bi-x-square"></i>';
+      editButton.classList.replace('btn-warning', 'btn-secondary');
+      postColumn.setAttribute('contenteditable', 'true');
+      udpateButton.classList.remove('d-none');
+      isBeingEdited = true;
+    }
+
+    document.addEventListener('click', (event) => {
+      event.stopPropagation();
+      if (!tr.contains(event.target)) cancelEditing();
+    });
+
+    const editButton = tr.querySelector('.btn-warning');
+    editButton.addEventListener('click', () => {
+      if (isBeingEdited) cancelEditing();
+      else enableEditing();
+    });
+
+  }
+
+  enableRowDeleteAction = ({ initialState, id, tr }) => {
+    const delButton = tr.querySelector('.btn-danger');
+    delButton.addEventListener('click', () => this.onDeletePost({ post: initialState.post, id }));
+  }
+
   createRowHtmlElement = ({ post, id }) => {
     const tr = document.createElement('tr');
     tr.innerHTML = `
       <td>${id}</td>
-      <td>${post}</td>
+      <td class="js-post-col">${post}</td>
       <td class="d-flex justify-content-end gap-2">
-      <button class="btn btn-warning btn-sm"><i class="bi bi-pencil-square"></i></i></button>
+      <button class="btn btn-success btn-sm d-none"><i class="bi bi-check2-square"></i></button>
+      <button class="btn btn-warning btn-sm"><i class="bi bi-pencil-square"></i></button>
       <button class="btn btn-danger btn-sm"><i class="bi bi-trash3"></i></button>
     </td>`;
 
-    const delButton = tr.querySelector('.btn-danger');
-    delButton.addEventListener('click', () => this.onDeletePost({ post, id }));
-
-    const updateButton = tr.querySelector('.btn-warning');
-    updateButton.addEventListener('click', () => {
-      console.log({id});
-    });
+    const actionProps = { id, tr, initialState: { post } }
+    this.enableRowDeleteAction(actionProps);
+    this.enableRowEditAction(actionProps);
 
     return tr;
   }
