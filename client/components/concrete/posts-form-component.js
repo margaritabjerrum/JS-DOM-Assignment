12f7ea473b5dd7c5d28f6e62dcc75df31b747094
margaritabjerrum <margarita.juskyte@gmail.com>
2022-12-30 09:52:02 +0200

    feat: added name input to form

--- client/components/concrete/posts-form-component.js
+++ client/components/concrete/posts-form-component.js
@@ -5,18 +5,23 @@ class PostsFormComponent {
     this.htmlElement = document.createElement('form');
     this.htmlElement.innerHTML = `
       <div class="mb-3">
-        <label for="posts" class="form-label">Write Your Post Here:</label>
+        <label for="name" class="form-label">Name:</label>
+        <input type="text" class="form-control" id="name">
+      </div>
+      <div class="mb-3">
+        <label for="posts" class="form-label">Your Message:</label>
         <textarea class="form-control" id="posts" name="posts" rows="3"></textarea>
       </div>
-      <button type="submit" class="btn btn-primary">Post</button>`
+      <button type="submit" class="btn btn-primary">Post message</button>`
 
     this.htmlElement.addEventListener('submit', async (event) => {
       event.preventDefault();
 
       const formData = new FormData(event.target);
+      const name = formData.get('name');
       const post = formData.get('posts');
 
-      onSubmit({ post });
+      onSubmit({ name, post });
       event.target.reset();
     })
 
